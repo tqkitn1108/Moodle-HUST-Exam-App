7c99e21d7a9e6420c05545110e4e498b5e57f98d
tqk_tn03 <kientaquang11@gmail.com>
2023-04-27 22:28:52 +0700

    Create Project

---
+++ src/main/java/start/StartApp.java
@@ -0,0 +1,23 @@
+package start;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Example.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
