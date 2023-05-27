7a2d0c0b03ece93f4cf6b2f252d233386722917c
TNIs03 <daudanhthaiqp@gmail.com>
2023-05-27 10:58:50 +0700

    add data interact files and database

--- src/main/java/models/Quiz.java
+++ src/main/java/models/Quiz.java
@@ -1,89 +1,24 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Quiz {
-    private Integer quizId;
-    private String title;
+    private String quizID;
+    private String quizTitle;
 
-    public static class MetaData {
-        public static final String TABLE_NAME = "quizzes";
-        public static final String QUIZ_ID = "quiz_id";
-        public static final String TITLE = "title";
+    public String getQuizID() {
+        return quizID;
+    }
+    public String getQuizTitle() {
+        return quizTitle;
+    }
+    public void setQuizID(String quizID) {
+        this.quizID = quizID;
+    }
+    public void setQuizTitle(String quizTitle) {
+        this.quizTitle = quizTitle;
+    }
+    Quiz(String quizID, String quizTitle) {
+        this.quizID = quizID;
+        this.quizTitle = quizTitle;
     }
 
-    public Quiz() {
-    }
-
-    public Quiz(String title) {
-        this.title = title;
-    }
-
-    public Integer getQuizId() {
-        return quizId;
-    }
-
-    public void setQuizId(Integer quizId) {
-        this.quizId = quizId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Quiz{" + "quizId=" + quizId + ", title=" + title + '}';
-    }
-
-    // ----------------------------------------------------------------
-    public static void createTable() { // Vid #18, #19
-        try {
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public int save() { // Vid #23, #24
-        return -1;
-    }
-
-    public boolean save(ArrayList<Question> questions) { // Vid #25
-        boolean flag = false;
-        this.quizId = this.save();
-
-        for(Question q: questions) {
-            q.save();
-        }
-
-        return flag;
-    }
-
-    public static void getAll() { // Vid #44
-        Map<Quiz, List<Question>> quizzes = new HashMap<>();
-        Quiz key = null;
-
-    }
-
-    // Map một Quiz với số câu hỏi của quiz đấy
-    public static Map<Quiz, Integer> getAllWithQuestionCount() { // Vid #51
-        Map<Quiz, Integer> quizzes = new HashMap<>();
-        Quiz key = null;
-
-        return quizzes;
-    }
-
-    public List<Question> getQuestions() {
-        List<Question> quiz = new ArrayList<>();
-
-        return quiz;
-    }
 }
