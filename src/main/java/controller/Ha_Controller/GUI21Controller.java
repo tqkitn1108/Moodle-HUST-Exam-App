209846934fce5671080930aac6e4677a9293e68c
nguyenha-meiii <nguyenthuhat1k60@gmail.com>
2023-05-28 23:32:50 +0700

    Update Bai 2

--- src/main/java/controller/Ha_Controller/GUI21Controller.java
+++ src/main/java/controller/Ha_Controller/GUI21Controller.java
@@ -3,13 +3,20 @@ package controller.Ha_Controller;
 import io.github.palexdev.materialfx.controls.MFXCheckbox;
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeView;
 
-public class GUI21Controller {
+
+
+
+public class GUI21Controller  {
 
     @FXML
     private TabPane tabPane;
 
     @FXML
     private MFXCheckbox checkBox;
+
+    @FXML
+    private TreeView<String> treeView;
 
 }
