a0d6d56b9fcd84f65b9d47998677edce49d1608c
dell <hungnguyen1972k3@gmail.com>
2023-05-29 22:37:21 +0700

    abcdefghijkmn

---
+++ src/main/java/controller/GUI34controller.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GUI34controller extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI3.4.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
